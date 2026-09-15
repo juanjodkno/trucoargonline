90ad41626f888df2da77badc91cfbae4227b614a
juan jose <juanjodkno@gmail.com>
2026-09-15 15:57:25 -0300

    Prueba detector de actualizacion

--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // src/server.ts
+// PRUEBA DE DEPLOY PARA DETECTOR DE ACTUALIZACION
 const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
 const path_1 = __importDefault(require("path"));
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 // src/server.ts
+// PRUEBA DE DEPLOY PARA DETECTOR DE ACTUALIZACION
 import express from 'express';
 import http from 'http';
 import path from 'path';
